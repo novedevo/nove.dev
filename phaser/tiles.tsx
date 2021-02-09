a626e5869addc1188395772346db4f7672a3c67b
Devon Burnham <novedevo@gmail.com>
2021-02-09 13:39:07 -0800

    phaser update

---
+++ phaser/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="8" tileheight="8" tilecount="4" columns="4">
+ <image source="tutorial/tiles.png" width="32" height="8"/>
+</tileset>
--- phaser/tutorial/test.js
+++ phaser/tutorial/test.js
@@ -1,18 +1,16 @@
-
-
-
 var config = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     width: 128,
     height: 128,
-    pixelArt:true,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    pixelArt: true,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
-            debug: false,
+            gravity: { y: 150 },
+            debug: false
         }
-
     },
     scene: {
         preload: preload,
@@ -21,29 +19,99 @@ var config = {
     }
 };
 
-var player;
-var ground;
-var level;
-var levelGrid;
+var maxSpeed = 75;
+var jumpSpeed = 100;
+var accel = 100;
+var friction = 0.5;
+var startRunVelocity = 30;
+var keysHeld = [];
+
 
 var game = new Phaser.Game(config);
+var player;
+var controls = Phaser.Input.Keyboard;
 
 function preload() {
-
-    this.load.image('tiles', 'tiles.png');
-    this.load.tilemapTiledJSON('map', 'test_level.json');
+    this.load.image('tiles', 'assets/tiles.png');
+    this.load.tilemapTiledJSON('map', 'assets/untitled.json');
+    this.load.image('bomb', 'assets/bomb.png');
 }
 
 function create() {
-    var map = this.make.tilemap({key:'map'});
+    var map = this.make.tilemap({ key: 'map' });
 
-    var layer = map.createLayer(0, this.tiles, 0, 0);
+    var tileset = map.addTilesetImage('tiles', 'tiles');
 
-    ground = this.physics.add.staticGroup();
+    var tile_layer = map.createLayer(0, tileset, 0, 0);
+
+    //var map = this.make.tilemap({ key: 'map', tileWidth: 16, tileHeight: 16 });
+    //var tileset = map.addTilesetImage("tileset-images", 'tiles');
+    //var tile_layer = map.createLayer('walls', tileset, 0, 0);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    player = this.physics.add.sprite(64, 16, 'bomb').setScale(0.5);
+    player.setCollideWorldBounds(true);
+    player.setFriction(friction);
+
+    ground = this.physics.add.group();
+    ground.create(tile_layer);
+
+    ground.getChildren()[0].setFrictionX(0.5);
+
+    // Set collision with player (can also be a group)
+    tile_layer.setCollisionByExclusion([-1]);
+    this.physics.add.collider(player, tile_layer);
+
+    
+
+    
+}
+
+function update(time, delta) {
+
+    if (controls.JustDown(cursors.left)){
+        player.setVelocityX(-startRunVelocity);
+    }
+    if (controls.JustDown(cursors.right)){
+        player.setVelocityX(startRunVelocity);
+    }
 
 
+    if (cursors.left.isDown)
+    {
+            if (Math.abs(player.body.velocity.x) > maxSpeed){
+                player.setAccelerationX(0);
+            }
+            else {
+                player.setAccelerationX(-accel);
+            }
+        
+    }
+    else if (cursors.right.isDown)
+    {
+        if (Math.abs(player.body.velocity.x) > maxSpeed){
+            player.setAccelerationX(0);
+        }
+        else {
+            player.setAccelerationX(accel);
+        }
+    }
+    else
+    {
+        player.setAccelerationX(0);
+        keysHeld = []
+    }
+
+    if (cursors.up.isDown && player.body.blocked.down)
+    {
+        player.setVelocityY(-jumpSpeed);
+    }
 
 }
 
-function update() {
+function frictionCalc(velocity){
+    console.log(-friction*velocity);
+    return -friction*velocity;
+    
 }
