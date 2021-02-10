6ae9239a2555394a0a1a33e548984602c53d6595
Devon Burnham <novedevo@gmail.com>
2021-02-09 19:31:27 -0800

    optimizations

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+if (localStorage.getItem('data-theme')) {
+    document.documentElement.setAttribute('data-theme', localStorage.getItem('data-theme'));
+}
 
 function changeTheme() {
     if (document.documentElement.getAttribute('data-theme') === 'dark') {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('data-theme', 'light');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('data-theme', 'dark');
     }
 }
