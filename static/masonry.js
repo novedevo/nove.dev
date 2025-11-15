0351d4ebaab8d50712d57767ca2d38175554a623
Devon Sawatsky <devon@nove.dev>
2025-11-15 02:33:16 +0000

    delete all the old stuff

--- static/masonry.js
+++
@@ -1,8 +0,0 @@
-if (!CSS.supports('grid-template-columns', 'masonry')) {
-    var elem = document.querySelector('.projects');
-    var msnry = new Masonry(elem, {
-        itemSelector: '.project',
-        columnWidth: 10,
-        fitWidth: true,
-    });
-}
